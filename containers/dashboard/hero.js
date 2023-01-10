6addb58c39fac92c3c4344f36611ed1c3867fd10
reymondpamelar <reymondpamelar@gmail.com>
2023-01-10 00:22:01 -0800

    added image and bouncing middle dot

--- containers/dashboard/hero.js
+++ containers/dashboard/hero.js
@@ -1,18 +1,47 @@
+import * as Icon from "react-feather"
+import DarkModeToggle from "../../components/dark-mode/toggle";
+
 export default function Hero(){
 
     return(
         <div className={'h-screen flex items-end relative'}>
-            <div className={'text-8xl m-10 border-b border-black mb-10 w-full'}>
-                <div className={'pb-16 grid grid-cols-3'}>
-                    <div className={'col-span-2 font-neueBold flex items-end pr-16'}>
-                        Meet The Interns
+            <div className={'absolute top-6 right-6 z-50 '}>
+                <DarkModeToggle/>
+            </div>
+            <div className={'absolute w-screen h-screen opacity-90 blur-[2px]'}>
+                <img src={'/carlsbad-office.png'} className={' object-cover h-full w-full opacity-50 brightness-90 '}/>
+            </div>
+            <div className={'absolute w-screen h-screen flex justify-center items-center'}>
+                <div className={'p-10 group'}>
+                    <div className={'border-b border-white px-2 mb-40 relative'}>
+                        <div className={'group-hover:scale-[2.5] group-hover:-translate-y-3 transition ease-in-out duration-500'}>
+                            <div className={'animate-bounce'}>
+                                <div className={'animate-bounce '}>
+                                    <div className={'bg-white rounded-full p-2 animate-bounce'}>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className={'text-lg space-y-8'}>
-                        <div className={'border-b border-black pb-8'}>
-                            TRU DATA
+                </div>
+            </div>
+            <div className={'z-10 text-8xl m-10 border-b border-black dark:border-zinc-400 mb-10 w-full'}>
+                <div className={'pb-16 grid grid-cols-3'}>
+                    <div className={'col-span-2 font-neueBold grid items-end pr-16'}>
+                        <div className={'text-2xl font-neueBook flex items-center'}>
+                            <Icon.MapPin className={'mr-2'}/> Carlsbad
+                        </div>
+                        <div>
+                            Meet The Interns
+                        </div>
+                    </div>
+                    <div className={'text-sm space-y-8 font-neueBook'}>
+                        <div className={'border-b border-black dark:border-zinc-400 pb-8'}>
+                            <img src={'/final-trudata-logo white.webp'} className={'mix-blend-exclusion'}/>
                         </div>
                         <div className={'space-y-3'}>
-                            <div className={'font-neueBold text-xl'}>
+                            <div className={'font-neueBold text-lg'}>
                                 Software Engineering
                             </div>
                             <div className={''}>
@@ -24,7 +53,7 @@ export default function Hero(){
                         </div>
 
                         <div className={'space-y-3'}>
-                            <div className={'font-neueBold text-xl'}>
+                            <div className={'font-neueBold text-lg'}>
                                 Data Engineering
                             </div>
                             <div className={''}>
