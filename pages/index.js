11f06351bd4763d31451696e06778fbb841ce407
reymondpamelar <reymondpamelar@gmail.com>
2023-01-09 18:47:16 -0800

    Added fonts and old tailwind config

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,7 @@ export default function Home() {
     return (
         <div>
             <DashboardLayout>
-                <div>
 
-                </div>
             </DashboardLayout>
         </div>
     )
